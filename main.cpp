8bad3bae16ffb0de6b8a8272f76cb5246276bd0b
Roshan <bhattaroshan@gmail.com>
2025-01-02 15:07:34 +0545

    added dark theme by default

--- main.cpp
+++ main.cpp
@@ -2,9 +2,31 @@
 
 #include <QApplication>
 
+void setDarkTheme(QApplication &app) {
+    QPalette darkPalette;
+    darkPalette.setColor(QPalette::Window, QColor(53, 53, 53));
+    darkPalette.setColor(QPalette::WindowText, Qt::white);
+    darkPalette.setColor(QPalette::Base, QColor(42, 42, 42));
+    darkPalette.setColor(QPalette::AlternateBase, QColor(66, 66, 66));
+    darkPalette.setColor(QPalette::ToolTipBase, Qt::white);
+    darkPalette.setColor(QPalette::ToolTipText, Qt::white);
+    darkPalette.setColor(QPalette::Text, Qt::white);
+    darkPalette.setColor(QPalette::Button, QColor(53, 53, 53));
+    darkPalette.setColor(QPalette::ButtonText, Qt::white);
+    darkPalette.setColor(QPalette::BrightText, Qt::red);
+    darkPalette.setColor(QPalette::Link, QColor(42, 130, 218));
+    darkPalette.setColor(QPalette::Highlight, QColor(42, 130, 218));
+    darkPalette.setColor(QPalette::HighlightedText, Qt::black);
+
+    app.setPalette(darkPalette);
+
+}
+
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    setDarkTheme(a);
     MainWindow w;
     w.show();
     return a.exec();
